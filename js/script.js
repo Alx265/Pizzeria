d9f792a5ccdfe1a776ee43d0a19b2cce7dde96e4
Marcin <mwawreczko265@gmail.com>
2021-06-04 16:04:50 +0200

    Header change

--- js/script.js
+++ js/script.js
@@ -14,3 +14,12 @@ function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
+
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#main-nav').style.opacity = 0.8;
+  } else {
+    document.querySelector('#main-nav').style.opacity = 1;
+  }
+});
